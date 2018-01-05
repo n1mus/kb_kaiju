e28e1f1ee252dbf4730f72e90c627ad191e9c4d3
dcchivian <dcchivian@lbl.gov>
2018-01-05 13:08:50 -0800

    install ref data

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -27,33 +27,33 @@ elif [ "${1}" = "init" ] ; then
   tar -xzf kaiju_index.tgz
   rm kaiju_index.tgz
 
-#  echo "downloading: http://kaiju.binf.ku.dk/database/kaiju_index_pg.tgz"
-#  mkdir -p /data/kaijudb/kaiju_index_pg
-#  cd /data/kaijudb/kaiju_index_pg
-#  wget -q http://kaiju.binf.ku.dk/database/kaiju_index_pg.tgz
-#  tar -xzf kaiju_index_pg.tgz
-#  rm kaiju_index_pg.tgz
+  echo "downloading: http://kaiju.binf.ku.dk/database/kaiju_index_pg.tgz"
+  mkdir -p /data/kaijudb/kaiju_index_pg
+  cd /data/kaijudb/kaiju_index_pg
+  wget -q http://kaiju.binf.ku.dk/database/kaiju_index_pg.tgz
+  tar -xzf kaiju_index_pg.tgz
+  rm kaiju_index_pg.tgz
 
-#  echo "downloading: http://kaiju.binf.ku.dk/database/kaiju_index_nr.tgz"
-#  mkdir -p /data/kaijudb/kaiju_index_nr
-#  cd /data/kaijudb/kaiju_index_nr
-#  wget -q http://kaiju.binf.ku.dk/database/kaiju_index_nr.tgz
-#  tar -xzf kaiju_index_nr.tgz
-#  rm kaiju_index_nr.tgz
+  echo "downloading: http://kaiju.binf.ku.dk/database/kaiju_index_nr.tgz"
+  mkdir -p /data/kaijudb/kaiju_index_nr
+  cd /data/kaijudb/kaiju_index_nr
+  wget -q http://kaiju.binf.ku.dk/database/kaiju_index_nr.tgz
+  tar -xzf kaiju_index_nr.tgz
+  rm kaiju_index_nr.tgz
 
-#  echo "downloading: http://kaiju.binf.ku.dk/database/kaiju_index_nr_euk.tgz"
-#  mkdir -p /data/kaijudb/kaiju_index_nr_euk
-#  cd /data/kaijudb/kaiju_index_nr_euk
-#  wget -q http://kaiju.binf.ku.dk/database/kaiju_index_nr_euk.tgz
-#  tar -xzf kaiju_index_nr_euk.tgz
-#  rm kaiju_index_nr_euk.tgz
+  echo "downloading: http://kaiju.binf.ku.dk/database/kaiju_index_nr_euk.tgz"
+  mkdir -p /data/kaijudb/kaiju_index_nr_euk
+  cd /data/kaijudb/kaiju_index_nr_euk
+  wget -q http://kaiju.binf.ku.dk/database/kaiju_index_nr_euk.tgz
+  tar -xzf kaiju_index_nr_euk.tgz
+  rm kaiju_index_nr_euk.tgz
 
   cd /data/kaijudb
-  if [ -s "/data/kaijudb/kaiju_index/kaiju_db.fmi" ] ; then
+#  if [ -s "/data/kaijudb/kaiju_index/kaiju_db.fmi" ] ; then
 #  if [ -s "/data/kaijudb/kaiju_index/kaiju_db.fmi" -eq 15851683711 -a -s "/data/kaijudb/kaiju_index_pg/kaiju_db.fmi" -eq 12737853741 -a -s "/data/kaijudb/kaiju_index_nr/kaiju_db_nr.fmi" -eq 45220614796 -a -s "/data/kaijudb/kaiju_index_nr_euk/kaiju_db_nr_euk.fmi" -eq 51503967892 ] ; then
-#  if [ -s "/data/kaijudb/kaiju_index/kaiju_db.fmi" -a -s "/data/kaijudb/kaiju_index_pg/kaiju_db.fmi" -a -s "/data/kaijudb/kaiju_index_nr/kaiju_db_nr.fmi" -a -s "/data/kaijudb/kaiju_index_nr_euk/kaiju_db_nr_euk.fmi" ] ; then
+  if [ -s "/data/kaijudb/kaiju_index/kaiju_db.fmi" -a -s "/data/kaijudb/kaiju_index_pg/kaiju_db.fmi" -a -s "/data/kaijudb/kaiju_index_nr/kaiju_db_nr.fmi" -a -s "/data/kaijudb/kaiju_index_nr_euk/kaiju_db_nr_euk.fmi" ] ; then
     echo "DATA DOWNLOADED SUCCESSFULLY"
-#    touch /data/__READY__
+    touch /data/__READY__
   else
     echo "Init failed"
   fi  
