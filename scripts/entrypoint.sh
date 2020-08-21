fa0bcf8254ddbbe5c13f926c1d22b54cf8f4c676
Sean Jungbluth <jungbluth.sean@gmail.com>
2020-08-20 19:54:40 -0700

    Databases and binary updates

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -11,6 +11,7 @@ fi
 if [ $# -eq 0 ] ; then
   sh ./scripts/start_server.sh
 elif [ "${1}" = "test" ] ; then
+  head -10 /data/kaijudb/refseq/nodes.dmp
   echo "Run Tests"
   make test
 elif [ "${1}" = "async" ] ; then
@@ -21,41 +22,45 @@ elif [ "${1}" = "init" ] ; then
   mkdir -p /data/kaijudb
   cd /data/kaijudb
 
-  echo "downloading: http://kaiju.binf.ku.dk/database/kaiju_db_refseq_2019-06-25.tgz"
-  mkdir -p /data/kaijudb/
+  echo "downloading: http://kaiju.binf.ku.dk/database/kaiju_db_refseq_2020-05-25.tgz"
+  mkdir -p /data/kaijudb/refseq
   cd /data/kaijudb/
-  wget -q http://kaiju.binf.ku.dk/database/kaiju_db_refseq_2019-06-25.tgz
-  tar -xzf kaiju_db_refseq_2019-06-25.tgz
+  wget -q http://kaiju.binf.ku.dk/database/kaiju_db_refseq_2020-05-25.tgz
+  tar -xzf kaiju_db_refseq_2020-05-25.tgz
+  mv kaiju_db_refseq.fmi refseq/kaiju_db_refseq.fmi
   mv names.dmp refseq/names.dmp
   mv nodes.dmp refseq/nodes.dmp
-  rm kaiju_db_refseq_2019-06-25.tgz
+  rm kaiju_db_refseq_2020-05-25.tgz
 
-  echo "downloading: http://kaiju.binf.ku.dk/database/kaiju_db_progenomes_2019-06-25.tgz"
-  mkdir -p /data/kaijudb/
+  echo "downloading: http://kaiju.binf.ku.dk/database/kaiju_db_progenomes_2020-05-25.tgz"
+  mkdir -p /data/kaijudb/progenomes
   cd /data/kaijudb/
-  wget -q http://kaiju.binf.ku.dk/database/kaiju_db_progenomes_2019-06-25.tgz
-  tar -xzf kaiju_db_progenomes_2019-06-25.tgz
+  wget -q http://kaiju.binf.ku.dk/database/kaiju_db_progenomes_2020-05-25.tgz
+  tar -xzf kaiju_db_progenomes_2020-05-25.tgz
+  mv kaiju_db_progenomes.fmi progenomes/kaiju_db_progenomes.fmi
   mv names.dmp progenomes/names.dmp
   mv nodes.dmp progenomes/nodes.dmp
-  rm kaiju_db_progenomes_2019-06-25.tgz
+  rm kaiju_db_progenomes_2020-05-25.tgz
 
-  echo "downloading: http://kaiju.binf.ku.dk/database/kaiju_db_nr_2019-06-25.tgz"
-  mkdir -p /data/kaijudb/
+  echo "downloading: http://kaiju.binf.ku.dk/database/kaiju_db_nr_2020-05-25.tgz"
+  mkdir -p /data/kaijudb/nr
   cd /data/kaijudb/
-  wget -q http://kaiju.binf.ku.dk/database/kaiju_db_nr_2019-06-25.tgz
-  tar -xzf kaiju_db_nr_2019-06-25.tgz
+  wget -q http://kaiju.binf.ku.dk/database/kaiju_db_nr_2020-05-25.tgz
+  tar -xzf kaiju_db_nr_2020-05-25.tgz
+  mv kaiju_db_nr.fmi nr/kaiju_db_nr.fmi
   mv names.dmp nr/names.dmp
   mv nodes.dmp nr/nodes.dmp
-  rm kaiju_db_nr_2019-06-25.tgz
+  rm kaiju_db_nr_2020-05-25.tgz
 
-  echo "downloading: http://kaiju.binf.ku.dk/database/kaiju_db_nr_euk_2019-06-25.tgz"
-  mkdir -p /data/kaijudb/
+  echo "downloading: http://kaiju.binf.ku.dk/database/kaiju_db_nr_euk_2020-05-25.tgz"
+  mkdir -p /data/kaijudb/nr_euk
   cd /data/kaijudb/
-  wget -q http://kaiju.binf.ku.dk/database/kaiju_db_nr_euk_2019-06-25.tgz
-  tar -xzf kaiju_db_nr_euk_2019-06-25.tgz
+  wget -q http://kaiju.binf.ku.dk/database/kaiju_db_nr_euk_2020-05-25.tgz
+  tar -xzf kaiju_db_nr_euk_2020-05-25.tgz
+  mv kaiju_db_nr_euk.fmi nr_euk/kaiju_db_nr_euk.fmi
   mv names.dmp nr_euk/names.dmp
   mv nodes.dmp nr_euk/nodes.dmp
-  rm kaiju_db_nr_euk_2019-06-25.tgz
+  rm kaiju_db_nr_euk_2020-05-25.tgz
 
   cd /data/kaijudb
 
